50fa426a66e2e1a2a526df843d505c21a3588c9c
badarianof <badarianofgafoor01@gmail.com>
2026-01-28 18:42:51 +0000

    Revert "Add student society list screen and connect Supabase backend (#1)"
    This reverts commit e2b12b16a68b13368b852427e0b3c53bdbbd9e50.

--- App.js
+++ App.js
@@ -1,47 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import SocietyListScreen from "./src/components/screens/student/SocietyListScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Stack = createNativeStackNavigator();
-
-export const App = () => {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SocietyListScreen">
-        <Stack.Screen
-          name="SocietyListScreen"
-          component={SocietyListScreen}
-          options={{ title: "Society" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>HELLO</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
-
-// import { View, Text, StyleSheet } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.text}>IT WORKS</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: 'red',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   text: {
-//     color: 'white',
-//     fontSize: 30,
-//     fontWeight: 'bold',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
