6b116a129f1112ff7be463aa8e74c1166f3ec33a
badarianof <badarianofgafoor01@gmail.com>
2026-04-03 13:10:23 +0100

    temporarily commented out student login

--- src/components/screens/RoleSelectionScreen.js
+++ src/components/screens/RoleSelectionScreen.js
@@ -7,8 +7,8 @@ export default function RoleSelectScreen({ navigation }) {
 
       <Button
         title="Student"
-        onPress={() => navigation.navigate("StudentLogin")}
-        // onPress={() => navigation.navigate("StudentHomeScreen")}
+        // onPress={() => navigation.navigate("StudentLogin")}
+        onPress={() => navigation.navigate("StudentTabs")}
       />
 
       <Button
