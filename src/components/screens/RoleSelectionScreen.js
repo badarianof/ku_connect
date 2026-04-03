732bfcc111b97c253aaf7f604cbf870297a3fe5a
badarianof <badarianofgafoor01@gmail.com>
2026-04-03 14:45:32 +0100

    show events from followed societies on societies tab

--- src/components/screens/RoleSelectionScreen.js
+++ src/components/screens/RoleSelectionScreen.js
@@ -7,8 +7,8 @@ export default function RoleSelectScreen({ navigation }) {
 
       <Button
         title="Student"
-        // onPress={() => navigation.navigate("StudentLogin")}
-        onPress={() => navigation.navigate("StudentTabs")}
+        onPress={() => navigation.navigate("StudentLogin")}
+        // onPress={() => navigation.navigate("StudentTabs")}
       />
 
       <Button
