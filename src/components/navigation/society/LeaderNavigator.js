8fb1faff3c985c03daa2b66233fc6048ffd0aaef
badarianof <badarianofgafoor01@gmail.com>
2026-04-13 19:32:05 +0100

    apply theme to edit event screen

--- src/components/navigation/society/LeaderNavigator.js
+++ src/components/navigation/society/LeaderNavigator.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LeaderTabs from "./LeaderTabs";
 import EditEventScreen from "../../screens/society/EditEventScreen";
 import EditSocietyProfile from "../../screens/society/EditSocietyProfile";
+import { colors, tabBarStyle } from "../../../theme";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +17,15 @@ export default function LeaderNavigator() {
       <Stack.Screen
         name="EditEvent"
         component={EditEventScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          headerShown: true,
+          headerTitle: "Edit Event",
+          headerStyle: { backgroundColor: colors.background },
+          headerTintColor: colors.primary,
+          headerTitleStyle: { fontWeight: "bold" },
+          headerShadowVisible: false,
+        }}
       />
       <Stack.Screen
         name="EditSocietyProfile"
